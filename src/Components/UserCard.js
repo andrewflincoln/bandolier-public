58cfb5ad57103747126f5b9e5a14f6f897d796c5
Andrew Lincoln <andrewjlincoln@gmail.com>
2019-02-10 19:05:00 -0800

    moves user profile out to new card component

---
+++ src/Components/UserCard.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Text, View, ImageBackground, Image, Button, ScrollView} from 'react-native'
+import styles from '../styles'
+
+
+function UserCard(props) {
+   const {imgUrl} = props.user
+  return (
+
+    <View style={styles.profileInner}>    
+      <Image
+        style={styles.profilePic}
+        source={{uri: imgUrl}}
+        />
+
+      <Text style={styles.profileTextName}>{props.user.username}</Text>
+
+      {/* <Text style={styles.profileTextSectionHead}>Deal </Text> */}
+      <Text style={styles.profileTextDeal}>{props.user.deal}</Text>
+
+      {/* <Text style={styles.profileTextSectionHead}>Bio</Text> */}
+      <Text>{props.user.bio}</Text>
+
+
+      <View style={styles.genreInstrSection}>
+        <View>
+          <Text style={styles.profileTextSectionHead}>Genres </Text>
+          <Text>{props.user.genre_1}</Text>
+          {props.user.genre_2 ? 
+          <Text>{props.user.genre_2}</Text>
+          : null }
+          {props.user.genre_2 ? 
+          <Text>{props.user.genre_3}</Text>
+          : null }
+        </View>
+        <View style={styles.instrCol}>
+          <Text style={styles.profileTextSectionHead}>Instruments </Text>
+          <Text>{props.user.instr_1}</Text>
+          {props.user.instr_2 ? 
+          <Text>{props.user.instr_2}</Text>
+          : null }
+          {props.user.instr_3 ? 
+          <Text>{props.user.instr_3}</Text>
+          : null }
+        </View>
+      </View>
+
+      <Text style={styles.profileTextSectionHead}>Heroes </Text>
+      <Text>{props.user.heroes}</Text>
+
+      <Text style={styles.profileTextSectionHead}>Influences </Text>
+      <Text>{props.user.influences}</Text>
+    </View>  
+  )
+
+}
+
+export default UserCard
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,26 +1,30 @@
 import React from 'react'
-import {Text, View, ImageBackground, Image, Button} from 'react-native'
+import {Text, View, ImageBackground, Image, Button, ScrollView} from 'react-native'
 import { bindActionCreators } from 'redux';
 import {connect} from 'react-redux'
 import axios from 'axios'
 import styles from '../styles'
 import {SecureStore} from 'expo'
+import UserCard from './UserCard'
 
 const BASE_URL = `https://quiet-garden-92157.herokuapp.com`
 export default class UserProfile extends React.Component {
   constructor(props) {
     super(props)
 
+    
+
     this.state= {
+      userId: 6,   //dummy user login
       currentUser: {}
     }
   }
 
 
-  getUser (userId){
+  getUser (id){
     // axios.get(`http://TMA-2-Iapetus:3000/users/1`) //this doesn't work.
     // axios.get(`http://192.168.122.1:3000/users/1`)
-     axios.get(`${BASE_URL}/users/${userId}`)
+     axios.get(`${BASE_URL}/users/${id}`)
     .then(response => {
       this.setState({currentUser: response.data[0]})
 
@@ -30,26 +34,8 @@ export default class UserProfile extends React.Component {
 
 
 
-  componentWillMount = async () => {
-    // console.log('will mount')
-    // SecureStore.getItemAsync('token')
-    // .then(token => {
-    //   axios(`https://quiet-garden-92157.herokuapp.com/login`), {
-    //     method: 'get',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'Accept': 'application/json',
-    //       'Authorization': token ? `Bearer ${token}` : ''
-    //     }
-    //   }  
-    // })
-    // .then(response => {
-    //   this.getUser(response.data.id)
-    //   console.log(JSON.stringify(response))
-    // })
-    // const getEmail = SecureStore.getItemAsync('userEmail').then(result => console.log(result))
-    // let userEmail = getEmail()
-    this.getUser(1) //change with auth
+  componentWillMount = () => {
+    this.getUser(1) 
   }
 
   //function to randomly pick matching user
@@ -61,80 +47,35 @@ export default class UserProfile extends React.Component {
       <ImageBackground source={require('../guitars/IMG_20190208_065249773_HDR.jpg')} style={styles.imgBG}>
         <View style={styles.profileBG}>
           {/* Maybe this inner view is a separate card component? */}
-          <View style={styles.profileInner}>
-          
-     
-     
-        <Text>Welcome to the show, {this.state.currentUser.username}</Text>
-  
-          
-            <Image
-              style={styles.profilePic}
-              // source={{uri: 'http://www.thetrapset.net/wp-content/uploads/2014/12/carla_aLux-2.jpg'}}
-              source={{uri: this.state.currentUser.imgUrl}}
-             />
 
-            <Text style={styles.profileTextName}>{this.state.currentUser.username}</Text>
-
-            {/* <Text style={styles.profileTextSectionHead}>Deal </Text> */}
-            <Text style={styles.profileTextDeal}>{this.state.currentUser.deal}</Text>
-
-            {/* <Text style={styles.profileTextSectionHead}>Bio</Text> */}
-            <Text>{this.state.currentUser.bio}</Text>
+          <ScrollView>
+            <UserCard
+              user={this.state.currentUser}
+            />
 
 
-            <View style={styles.genreInstrSection}>
-              <View>
-                <Text style={styles.profileTextSectionHead}>Genres </Text>
-                <Text>{this.state.currentUser.genre_1}</Text>
-                {this.state.currentUser.genre_2 ? 
-                <Text>{this.state.currentUser.genre_2}</Text>
-                : null }
-                {this.state.currentUser.genre_2 ? 
-                <Text>{this.state.currentUser.genre_3}</Text>
-                : null }
-              </View>
-              <View style={styles.instrCol}>
-                <Text style={styles.profileTextSectionHead}>Instruments </Text>
-                <Text>{this.state.currentUser.instr_1}</Text>
-                {this.state.currentUser.instr_2 ? 
-                <Text>{this.state.currentUser.instr_2}</Text>
-                : null }
-                {this.state.currentUser.instr_3 ? 
-                <Text>{this.state.currentUser.instr_3}</Text>
-                : null }
-              </View>
-            </View>
-
-            <Text style={styles.profileTextSectionHead}>Heroes </Text>
-            <Text>{this.state.currentUser.heroes}</Text>
-
-            <Text style={styles.profileTextSectionHead}>Influences </Text>
-            <Text>{this.state.currentUser.influences}</Text>
-             
-            <View style={styles.playBar}>
-              <Button
-                title='Back'
-                onPress={() => { if(this.state.currentUser.id > 0) 
-                    this.getUser(this.state.currentUser.id -1); 
-                    console.log(this.state.currentUser.imgUrl) } }
-              />
-              <Button
-                title='Next'
-                onPress={() => { if(this.state.currentUser.id <5) 
-                    this.getUser(this.state.currentUser.id + 1); 
-                    SecureStore.getItemAsync('token')
-                    .then(result => (console.log(result)))  
-                  } }
-              />
-            </View>
-
+          </ScrollView>
+        
+          <View style={styles.playBar}>
+            <Button
+              title='Back'
+              onPress={() => { if(this.state.currentUser.id > 0) 
+                  this.getUser(this.state.currentUser.id -1); 
+                  console.log(this.state.currentUser.imgUrl) } }
+            />
+            <Button
+              title='Next'
+              onPress={() => { if(this.state.currentUser.id <5) 
+                  this.getUser(this.state.currentUser.id + 1); 
+      
+                } }
+            />
           </View> 
-     
+    
         </View>
       </ImageBackground>
-    )
-  }
+    )//return
+  }//render
 
 
 }
