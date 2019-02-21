d0057162a8fc9c95b08d03986ff2ac3837170c8e
Andrew Lincoln <andrewjlincoln@gmail.com>
2019-02-20 21:45:31 -0800

    UserCard now class component, plays song on render

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,65 +4,82 @@ import styles from '../styles'
 import {Audio} from 'expo'
 
 
-function UserCard(props) {
-   const {img_url} = props.user
-    // if (props.playItSam )
-    //   props.playItSam() //this comes from home, not getting it from playlist
-      //running when new card comes up but doesn't do anything
+export default class UserCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
 
 
 
+  // componentDidMount() {
+  //   console.log(this.props.user.url)
+  //   this.playUser(this.props.user.url)
+  // }
 
-  return (
+  playUser = async (url) => {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync({uri: url});
+      await soundObject.playAsync();
+    }
+    catch (error) {
+       console.log('An error occurred!', error)
+    }
+  }
+  render() {
 
-    <View style={styles.profileInner}>  
-      <Image
-        style={styles.profilePic}
-        source={{uri: img_url}}
-        />
-      <View style={styles.nameMatchBar}>
-        <Text style={styles.profileTextName}>{props.user.username}</Text>
-        <Text style={styles.profileTextName}>{props.user.match}</Text>
-      </View>
-      {/* <Text style={styles.profileTextSectionHead}>Deal </Text> */}
-      <Text style={styles.profileTextDeal}>{props.user.deal}</Text>
-
+    this.playUser(this.props.user.url)
+    return (
     
-      <View style={styles.genreInstrSection}>
-        <View>
-          <Text style={styles.profileTextSectionHead}>Genres </Text>
-          <Text>{props.user.genre_1}</Text>
-          {props.user.genre_2 ? 
-          <Text>{props.user.genre_2}</Text>
-          : null }
-          {props.user.genre_2 ? 
-          <Text>{props.user.genre_3}</Text>
-          : null }
+      <View style={styles.profileInner}>  
+        <Image
+          style={styles.profilePic}
+          source={{uri: this.props.user.img_url}}
+          />
+        <View style={styles.nameMatchBar}>
+          <Text style={styles.profileTextName}>{this.props.user.username}</Text>
+          <Text style={styles.profileTextName}>{this.props.user.match}</Text>
         </View>
-        <View style={styles.instrCol}>
-          <Text style={styles.profileTextSectionHead}>Instruments </Text>
-          <Text>{props.user.instr_1}</Text>
-          {props.user.instr_2 ? 
-          <Text>{props.user.instr_2}</Text>
-          : null }
-          {props.user.instr_3 ? 
-          <Text>{props.user.instr_3}</Text>
-          : null }
+        <Text style={styles.profileTextDeal}>{this.props.user.deal}</Text>
+
+      
+        <View style={styles.genreInstrSection}>
+          <View>
+            <Text style={styles.profileTextSectionHead}>Genres </Text>
+            <Text>{this.props.user.genre_1}</Text>
+            {this.props.user.genre_2 ? 
+            <Text>{this.props.user.genre_2}</Text>
+            : null }
+            {this.props.user.genre_2 ? 
+            <Text>{this.props.user.genre_3}</Text>
+            : null }
+          </View>
+          <View style={styles.instrCol}>
+            <Text style={styles.profileTextSectionHead}>Instruments </Text>
+            <Text>{this.props.user.instr_1}</Text>
+            {this.props.user.instr_2 ? 
+            <Text>{this.props.user.instr_2}</Text>
+            : null }
+            {this.props.user.instr_3 ? 
+            <Text>{this.props.user.instr_3}</Text>
+            : null }
+          </View>
         </View>
-      </View>
 
-      <Text style={styles.profileTextSectionHead}>Heroes </Text>
-      <Text>{props.user.heroes}</Text>
+        <Text style={styles.profileTextSectionHead}>Heroes </Text>
+        <Text>{this.props.user.heroes}</Text>
 
-      <Text style={styles.profileTextSectionHead}>Influences </Text>
-      <Text>{props.user.influences}</Text>
+        <Text style={styles.profileTextSectionHead}>Influences </Text>
+        <Text>{this.props.user.influences}</Text>
 
-      <Text style={styles.profileTextSectionHead}>Bio</Text>
-      <Text>{props.user.bio}</Text>
+        <Text style={styles.profileTextSectionHead}>Bio</Text>
+        <Text>{this.props.user.bio}</Text>
 
-    </View>  
-  )
+      </View>  
+    )
+  }
 
 }
-
-export default UserCard
